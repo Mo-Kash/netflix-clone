ed59ad52e381a87b2c97c7c9f88a9b0a92e09ccc
Mohit Kashyap <142307859+Mo-Kash@users.noreply.github.com>
2025-02-28 18:00:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useEffect } from 'react'
-import { auth } from './firebase'
+import { auth } from './firebase.js'
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
